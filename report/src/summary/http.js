e54c2ec3f505446d2448d728c2e321c593a483d1
QuentiumYT <quentin.lienhardt@gmail.com>
2025-09-09 16:20:28 +0200

    Better legacy?

--- report/src/summary/http.js
+++ report/src/summary/http.js
@@ -2,7 +2,7 @@
 const summary = (report) => {
   if (report) {
     // @ts-ignore use legacy value
-    const { grade } = report || report.scan;
+    const grade = report.grade ?? report.scan?.grade;
     if (grade) {
       return {
         httpGrade: grade,
