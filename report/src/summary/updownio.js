20fcb1a129269f99519ef1135744ebe51bc29030
QuentiumYT <quentin.lienhardt@gmail.com>
2025-09-10 10:43:24 +0200

    number can be 0

--- report/src/summary/updownio.js
+++ report/src/summary/updownio.js
@@ -3,7 +3,7 @@ const summary = (report) => {
   if (report) {
     const { uptime, metrics, uptimeGrade, apdexGrade } = report;
     const apdex = metrics?.apdex;
-    if (uptime) {
+    if (uptime !== undefined && uptime !== null) {
       return {
         apdex: apdex,
         apdexGrade: apdexGrade,
