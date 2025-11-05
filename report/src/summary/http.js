3a28a450e98e825b3daf4dd94e6adecd8a6f0008
QuentiumYT <quentin.lienhardt@gmail.com>
2025-11-05 17:28:20 +0100

    Fix weird timezones

--- report/src/summary/http.js
+++ report/src/summary/http.js
@@ -12,9 +12,10 @@ const summary = (report) => {
     // @ts-ignore use legacy value
     const date = report?.response_headers?.Date ?? report.scan?.responseHeaders?.date;
     if (grade) {
+      const dateFixTimezone = date ? date.replace(/ [A-Z]{3,4}$/, ' GMT') : null;
       return {
         httpGrade: grade,
-        httpDate: date ? new Date(date).toISOString() : undefined,
+        httpDate: new Date(dateFixTimezone).toISOString(),
       };
     }
   }
